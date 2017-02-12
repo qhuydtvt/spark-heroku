0d52ba380e15b51d9bcb33c162754529b2b20af4
huynq <qhuydtvt@gmail.com>
2017-02-13 00:34:01 +0700

    Initial commit

---
+++ src/main/java/Program.java
@@ -0,0 +1,20 @@
+/**
+ * Created by huynq on 2/12/17.
+ */
+
+import static spark.Spark.*;
+
+public class Program {
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        get("/hello", (req, res) -> "Hello Heroku World");
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+}
